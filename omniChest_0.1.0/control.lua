c4ce58af5cf7b80132e3694d5763ca4f85184696
SamuelS42 <samuelsanders42@gmail.com>
2018-01-29 23:00:04 -0500

    Added base for chest control logic

--- omniChest_0.1.0/control.lua
+++ omniChest_0.1.0/control.lua
@@ -8,28 +8,36 @@ script.on_event(defines.events.on_console_command,
 
 script.on_init(
  function()
-		-- createChest
-		game.print("This does too!")
 		global.masterChest = game.surfaces[1].create_entity{name="omni-inventory", position={0,0},}
+		global.slaveChests = {}
  end
 )
 
-script.on_event_name({defines.events.on_mod_item_opened},
-function(e)
-		-- createChest
-		if e.item == "omni-chest"then
-			game.print("This does  too!")
-			game.surfaces[1].create_entity{name="omni-inventory", position={0,0},}
+script.on_event(defines.events.on_tick,
+	function (e)
+		if e.tick % 30 == 0 then
+			--Sync slave chests to master chest here
 		end
- end
+	end
 )
 
-script.on_event({defines.events.on_gui_opened},
-function(e)
-		-- createChest
-		if e.entity.name == "omni-chest"then
-			game.print("This does  too!")
-			
+script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_entity},
+	function (e)
+		if e.created_entity.valid and e.created_entity.name == "omni-chest" then
+			table.insert(global.slaveChests, e.created_entity)
 		end
- end
+	end
+)
+
+script.on_event({defines.events.on_player_mined_entity, defines.events.on_robot_mined_entity, defines.events.on_entity_died},
+	function (e)
+		if e.entity.name == "omni-chest" then
+			for i, v in pairs(global.slaveChests) do
+				if v == e.entity then
+					table.remove(global.slaveChests, i)
+					break
+				end
+			end
+		end
+	end
 )
