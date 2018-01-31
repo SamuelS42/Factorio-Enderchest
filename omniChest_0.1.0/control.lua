366fcb995fc8e29911039ffa883ee010703fefdb
Evorionos <35902861+Evorionos@users.noreply.github.com>
2018-02-01 00:36:31 +0100

    WE DID IT

--- omniChest_0.1.0/control.lua
+++ omniChest_0.1.0/control.lua
@@ -96,18 +96,17 @@ script.on_event({defines.events.on_built_entity, defines.events.on_robot_built_e
 )
 
 script.on_event({defines.events.on_player_mined_entity, defines.events.on_robot_mined_entity, defines.events.on_entity_died},
-	function (e)
-		if e.entity.name == "omni-chest" then
-			for i, v in pairs(global.slaveChests) do
-				if v == e.entity then
-					thisCombinator = i
-					table.remove(global.slaveChests, i)
-					break
-				end
-			end
-			combinator = table.remove(global.combinators, i)
-			combinator.destroy()
-			
-		end
-	end
+    function (e)
+        if e.entity.name == "omni-chest" then
+            for i, v in pairs(global.slaveChests) do
+                if v == e.entity then
+                    thisCombinator = i
+                    table.remove(global.slaveChests, i)
+                    combinator = table.remove(global.combinators, i)
+                    combinator.destroy()
+                    break
+                end
+            end
+        end
+    end
 )
